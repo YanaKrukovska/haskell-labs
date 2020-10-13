d3f49892182257f71c232cf43adddcc11c02d6a8
Yana Krukovska <43723928+YanaKrukovska@users.noreply.github.com>
2020-10-13 21:43:33 +0300

    - Half of tasks from lab 6

---
+++ Krukovska06.hs
@@ -0,0 +1,116 @@
+{-# OPTIONS_GHC -Wall #-}
+module Krukovska06 where
+
+import Data.List(nub)
+
+type GraphS = (Int,[(Int,Int)])            
+type Graph  = [[Int]]
+
+--------------------- Test data - Graphs -------
+gr1S, gr2S:: GraphS
+gr1S = (5,[(0,1),(0,2),(0,3),(1,0),(1,3),(1,4),
+           (2,0),(2,4),(2,5),(3,0),(3,1),(4,1),(4,2),(5,2)])
+gr2S = (7,[(0,1),(0,3),(1,0),(1,2),(2,1),(2,3),(3,0),(3,2),
+           (4,5),(4,6),(5,4),(5,6), (6,4),(6,5)])
+
+gr1, gr2:: Graph
+gr1 = [[1,2,3],[0,3,4],[0,4,5],[0,1],[1,2],[2]]
+gr2 = [[1,3],[0,2],[1,3],[0,2],[5,6],[4,6],[4,5],[]]
+
+-- Task 1 ------------------------------------
+isOrdinary :: Graph -> Bool 
+isOrdinary gr = notElem False [x `elem` gr !! y | x <- nodes gr, y <- gr !! x]
+
+-- Task 2 ------------------------------------
+fromGraph :: Graph -> GraphS 
+fromGraph gr = (length gr - 1, edges gr)
+
+-- Task 3 ------------------------------------
+toGraph :: GraphS -> Graph
+toGraph (amount, graph) = [map snd y | y <- [filter (\n -> fst n == x) graph | x <- [0 .. amount]]]
+
+-- Task 4 ------------------------------------
+findShortest :: [[Int]] -> [Int]
+findShortest xs | length xs == 0 = []
+                | otherwise = snd (minimum [(length x, x) | x <- xs])
+
+shortWay :: Graph -> Int -> Int -> [Int]
+shortWay gr a b = findShortest ([reverse (x:xs)| i <- (allWays gr a), (x:xs) <- i, x == b])
+
+-- Task 5 ------------------------------------
+isConnecting :: Graph -> Bool 
+isConnecting gr = length gr == (length $ goNodes gr 0)
+
+-- Task 6 ------------------------------------
+components :: Graph -> [[Int]] 
+components = undefined
+
+-- Task 7 ------------------------------------
+eccentricity :: Graph -> Int -> Int 
+eccentricity = undefined
+
+-- Task 8 ------------------------------------
+findDiameter :: Graph -> Int 
+findDiameter = undefined
+
+findRadius :: Graph -> Int 
+findRadius = undefined
+
+-- Task 9 ------------------------------------
+findCenter :: Graph -> [Int] 
+findCenter = undefined
+
+-- Task 10 ------------------------------------
+shortWays :: Graph -> Int -> Int -> [[Int]] 
+shortWays = undefined
+
+--------------------- Lecture functions -------
+
+adj :: Graph -> Int -> [Int] 
+adj g v = g !! v
+
+nodes :: Graph -> [Int]
+nodes g = [0..(length g - 1)]
+
+edgeIn :: Graph -> (Int, Int) -> Bool
+edgeIn g (x,y) = elem y (g!!x)
+
+edges :: Graph -> [(Int,Int)]
+edges g = [(x, y) | x <- nodes g, y <- g !! x]  
+
+subsets :: [Int] -> [[Int]]
+subsets [] = [[]]
+subsets (x:xs) = let xss = subsets xs in map (x: ) xss  ++ xss 
+
+allEdges :: [Int] -> [(Int,Int)]
+allEdges xs = [(x,y) | x <- xs, y <- xs, x /= y]
+
+isClique :: Graph -> [Int] -> Bool
+isClique gr xs = let es = allEdges xs in  null $ filter (not . (edgeIn gr)) es 
+
+cliqueNum :: Graph -> Int
+cliqueNum gr = let  xss = subsets (nodes gr)
+                    qs = filter  (isClique gr) xss in   maximum $ map length qs
+
+allWays :: Graph -> Int -> [[[Int]]]
+allWays gr v = until condW (stepW gr) [[[v]]]
+
+condW :: ([[[Int]]]) -> Bool
+condW wss = null ( head wss)   
+
+stepW :: Graph -> [[[Int]]] -> [[[Int]]]
+stepW gr wss@(wsn:_) = [t:w | w@(x:xs) <- wsn, notElem x xs, t<- gr!!x] : wss
+stepW _ [] = []
+
+goNodes :: Graph -> Int -> [Int]
+goNodes gr v = snd $ until cond (oneStep gr) ([v],[])
+
+cond :: ([Int],[Int]) -> Bool
+cond (ns,_) = ns == []
+
+oneStep :: Graph -> ([Int], [Int]) -> ([Int],[Int]) 
+oneStep gr (ns,os) = let old = ns ++os
+                         ns1 = concatMap (gr !!) ns
+                         ns2 = filter (`notElem` old) ns1
+                         new = nub ns2
+ in (new,old)
