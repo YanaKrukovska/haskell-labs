afbee9e172a6c050e57b931dd81dd6682bb67a70
Yana Krukovska <43723928+YanaKrukovska@users.noreply.github.com>
2020-10-30 14:40:22 +0200

    - Finished remaining tasks in lab 7

--- Krukovska07.hs
+++ Krukovska07.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 module Krukovska07 where
 
+import Data.List
+
 data BinTreeM a = EmptyM 
                 | NodeM a Int (BinTreeM a) (BinTreeM a)
                    deriving (Show, Eq) 
@@ -71,43 +73,72 @@ findMin (NodeB _ trs) = findMin (head trs)
 
 findMax :: Btree a -> a
 findMax (NodeB ks []) = last ks
-findMax (NodeB _ trs)  = findMax (last trs)
+findMax (NodeB _ trs) = findMax (last trs)
 
 findBInform :: (Bounded a, Ord a) => Btree a ->  BInform a
 findBInform (NodeB ks []) = BInform 0 (head ks) (last ks)
 findBInform tr = BInform (findHeight tr) (findMin tr) (findMax tr)
 
 -- Task 7 ------------------------------------
-isBtree  :: (Bounded a, Ord a) => Int -> Btree a -> Bool 
+isBtree :: (Bounded a, Ord a) => Int -> Btree a -> Bool 
 isBtree = undefined
 
 -- Task 8 ------------------------------------
+bTreeIntoList :: (Ord a) => Btree a -> [a]
+bTreeIntoList (NodeB v tr) = if (length tr == 0) then v else bTreeIntoList (NodeB v (tail tr)) ++ bTreeIntoList (head tr)
+
 eqBtree :: (Bounded a, Ord a) => Int -> Btree a -> Btree a -> Bool 
-eqBtree = undefined
+eqBtree _ tr1 tr2 = sort (bTreeIntoList tr1) == sort (bTreeIntoList tr2)
 
 -- Task 9 ------------------------------------
 elemBtree :: Ord a => Btree a -> a -> Bool
-elemBtree = undefined
+elemBtree (NodeB [] _) _= False
+elemBtree (NodeB nodeV []) v = elem v nodeV
+elemBtree (NodeB nodeV (t:tr)) v = elemBtree t v || elemBtree (NodeB nodeV tr) v
 
 position :: Ord a => a -> [a] -> Int
-position = undefined
+position _ [] = 0
+position v (x:xs) = if (v == x || v < x) then 0 else 1 + position v xs
 
 -- Task 10 ------------------------------------
 insBtree :: Ord a => Int -> Btree a -> a -> Btree a
-insBtree = undefined
+insBtree t tr v | (isFull t tr) = let (l, k, b) = splitAtB t tr in insertInNode t (NodeB [k] [l, b]) v
+                | otherwise = insertInNode t tr v
 
 isFull :: Ord a => Int -> Btree a -> Bool
-isFull = undefined
+isFull size (NodeB v _) = 2 * size - 1 == length v
 
 insertKey :: Ord a => a -> [a] -> [a]
-insertKey = undefined
+insertKey v (x:xs)| v < x = [x] ++ (insertKey v xs)
+                  | v == x = [v, v] ++ xs
+                  | otherwise = [v, x] ++ xs
+insertKey v [] = [v]
 
-decomposeNodeB :: Ord a => a -> [a] -> [Btree a] -> 
-                        ([a], [a], [Btree a], Btree a, [Btree a])
-decomposeNodeB = undefined
+decomposeNodeB :: Ord a => a -> [a] -> [Btree a] -> ([a], [a], [Btree a], Btree a, [Btree a])
+decomposeNodeB v kl tl = let index = position v kl
+                             kl1 = [kl !! ks | ks <- [0..(index - 1)]]
+                             kl2 = [kl !! ks | ks <- [index..(length kl) - 1]]
+                             tl1 = [tl !! ts | ts <- [0..(index - 1)]]
+                             tl2 = [tl !! ts | ts <- [index + 1..(length tl) - 1]]
+                             bt = tl !! index
+                         in (kl1, kl2, tl1, bt, tl2)
 
 splitAtB :: Ord a => Int -> Btree a -> (Btree a, a, Btree a)
-splitAtB = undefined
+splitAtB t (NodeB kl tl) = let kl1 = [kl !! ks | ks <- [0..t - 2]]
+                               kl2 = [kl !! ks | ks <- [t..2 * t - 2]]
+                               tl1 = if (tl == []) then [] else [tl !! ts | ts <- [0..t - 1]]
+                               tl2 = if (tl == []) then [] else [tl !! ts | ts <- [t..2 * t - 1]]
+                           in (NodeB kl1 tl1, kl !! (t - 1), NodeB kl2 tl2)
+
+insertInNode :: Ord a => Int -> Btree a -> a -> Btree a
+insertInNode _ (NodeB ks []) v = NodeB (insertKey v ks) []
+insertInNode t (NodeB ks ts) v = if (isFull t bt) then
+                                    let (bt1, k, bt2) = splitAtB t bt
+                                        tree1 = if v <= k then (insertInNode t bt1 v) else bt1
+                                        tree2 = if v <= k then bt2 else (insertInNode t bt2 v)
+                                    in NodeB (kl1 ++ (k : kl2)) (tl1 ++ (tree1 : (tree2 : tl2)))
+                                 else NodeB ks (tl1 ++ ((insertInNode t bt v):tl2))
+                                 where (kl1, kl2, tl1, bt, tl2) = decomposeNodeB v ks ts
 
 --------------------- Test Data - Search Trees -------
 bm :: BinTreeM Char
