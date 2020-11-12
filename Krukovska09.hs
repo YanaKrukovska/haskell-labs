5c685a7ee3537a88934b64b41730df6c62561dca
Yana Krukovska <43723928+YanaKrukovska@users.noreply.github.com>
2020-11-12 22:29:50 +0200

    - Finished the remaining tasks in lab 9

--- Krukovska09.hs
+++ Krukovska09.hs
@@ -2,6 +2,7 @@
 module Krukovska09 where
 
 import Data.List
+import Data.Maybe(fromMaybe)
 import qualified Text.ParserCombinators.Parsec as P
 
 data RE = Null   |
@@ -55,21 +56,47 @@ labels :: [Transition] -> [Label]
 labels trx = nub ([labelX | (_, _, labelX) <- trx, labelX /= Eps])
 
 -- Task 5 -----------------------------------------
+getState :: Automation -> State
+getState (st, _, _) = st
+
+getSndState :: Transition -> State
+getSndState (_, endStates, _) = endStates
+
+acceptsDAHelper :: Automation -> State -> String -> Bool
+acceptsDAHelper daut state "" = isTerminal daut state
+acceptsDAHelper daut state (s:st) | isTerminal daut state = False 
+                                  | otherwise = if null res then False else acceptsDAHelper daut (getSndState(head res)) st
+                                where res = filter (\(_, _, label) -> (C s) == label) (transitionsFrom daut state)
+
 acceptsDA :: Automation -> String -> Bool
-acceptsDA = undefined
+acceptsDA daut st = acceptsDAHelper daut (getState daut) st
 
 -- Task 6 -----------------------------------------
-stStep  :: Automation -> State -> Label -> [State]
-setStep :: Automation -> [State] -> Label -> [State]
-closure :: Automation -> [State] -> [State]
+getNextState :: Transition -> State
+getNextState (_, nextState, _) = nextState
 
-stStep  = undefined
-setStep = undefined
-closure = undefined
+stStep :: Automation -> State -> Label -> [State]
+stStep naut st mc = map getNextState (filter (\(_, _, label) -> label == mc) (transitionsFrom naut st))
+
+setStep :: Automation -> [State] -> Label -> [State]
+setStep naut bs mc = concatMap (\st -> stStep naut st mc) bs
+
+notStateElem :: State -> [State] -> Bool
+notStateElem st stateList = null [x |x <- stateList, x == st]
+
+closure :: Automation -> [State] -> [State]
+closure naut st | null (filter (\x -> notStateElem x st) stepN) = st
+                | otherwise = sort (nub (closure naut (stepN ++ st)))
+                where stepN = setStep naut st Eps
 
 -- Task 7 -----------------------------------------
+acceptsHelper :: Automation -> String -> [State] -> Bool
+acceptsHelper _ _ [] = False
+acceptsHelper aut [] states = elem True (map (isTerminal aut) states)
+acceptsHelper aut (s:st) states = acceptsHelper aut st (setStep aut (closure aut states) (C s))     
+
 accepts :: Automation -> String -> Bool
-accepts = undefined
+accepts aut st = acceptsHelper aut st [(getState aut)]                    
 
 -- Task 8 -----------------------------------------
 makeNDA :: RE -> Automation
@@ -77,18 +104,122 @@ makeNDA re = (1, [2], sort transitionsAll)
   where (transitionsAll, _) = make (simplify re) 1 2 3
 
 make :: RE -> Int -> Int -> Int -> ([Transition], Int) 
-make = undefined
+make Null beg fin nxt = ([(beg, fin, Eps)], nxt)
+make (Plus _) _ _ _ = undefined
+make (Opt _) _ _ _ = undefined
+make (Term t) beg fin nxt = ([(beg, fin, C t)], nxt)
+make (Seq r1 r2) beg fin nxt = let s1 = make r1 beg nxt (nxt + 2)
+                                   s2 = make r2 (nxt + 1) fin (snd s1)
+                               in ((fst s1) ++ [(nxt, nxt + 1, Eps)] ++ (fst s2), (snd s2))
+make (Alt r1 r2) beg fin nxt = let s1 = make r1 nxt (nxt + 1) (nxt + 4)
+                                   s2 = make r2 (nxt + 2) (nxt + 3) (snd s1)
+                               in ([(beg, nxt, Eps)] ++ (fst s1) ++ [(nxt + 1, fin, Eps), (beg, nxt + 2, Eps)] ++ (fst s2) ++ [(nxt + 3, fin, Eps)], snd s2)
+make (Rep r1) beg fin nxt = let r2 = make r1 nxt (nxt + 1) (nxt + 2)
+                            in ([(beg, fin, Eps), (beg, nxt, Eps)] ++ (fst r2) ++ [(nxt + 1, nxt, Eps), (nxt + 1, fin, Eps)], (snd r2))
 
 -- Task 9 -----------------------------------------
+reg :: P.Parser RE
+reg = do r <- rexpr
+         P.eof
+         return r
+
+rexprHelper :: P.Parser RE
+rexprHelper = do _ <- P.char '|'
+                 r <- rfact
+                 return r
+
+altHelper :: [RE] -> RE
+altHelper [] = Null
+altHelper (x:[]) = x
+altHelper (x:xs) = Alt x (altHelper xs)
+
+rexpr :: P.Parser RE
+rexpr = do r1 <- rterm
+           r2 <- P.many (rexprHelper)
+           return (if null r2 then r1 else altHelper (r1:r2))
+
+sequenceHelper :: [RE] -> RE
+sequenceHelper [] = Null
+sequenceHelper (x:[]) = x
+sequenceHelper (x:xs) = Seq x (sequenceHelper xs)
+
+rterm :: P.Parser RE
+rterm = do r1 <- rfact
+           r2 <- P.many rfact
+           return (if null r2 then r1 else sequenceHelper(r1:r2))
+
+convert :: RE -> String -> RE
+convert r [] = r
+convert r (x:xs) | x == '*' = Rep (convert r xs)
+                 | x == '?' = Opt (convert r xs)
+                 | x == '+' = Plus (convert r xs)
+convert _ _ = Null
+
+rfact :: P.Parser RE
+rfact = do r <- prime
+           o <- P.many (P.oneOf "*?+")
+           return $ convert r (reverse o)
+
+prime :: P.Parser RE
+prime = (P.try rsymb) P.<|> primeHelper
+
+primeHelper :: P.Parser RE
+primeHelper = do _ <- P.char '('
+                 r <- rexpr
+                 _ <- P.char ')'
+                 return r
+
+rsymb :: (P.Parser) RE
+rsymb = do symbols <- P.noneOf "()|*+?"
+           return (Term symbols)
+
 parseReg :: String -> Maybe RE 
-parseReg = undefined
+parseReg str = case P.parse reg "" str of
+                Left _ -> Nothing
+                Right res -> Just res
 
 -- Task 10 -----------------------------------------
+getEndStates :: Automation -> [State]
+getEndStates (_, endStates, _) = endStates
+
+buildMetastate :: Automation -> MetaState -> [Label] -> [MetaState]
+buildMetastate aut msx l = map ((closure aut).(setStep aut msx)) l
+
+makeStep :: Automation -> ([MetaState], [MetaState], [MetaTransition]) -> ([MetaState], [MetaState], [MetaTransition])
+makeStep _ x@(_, [], _) = x
+makeStep aut (gmsx,(msx:bmsx), mtrx) = let l = nub (concatMap (labels.(transitionsFrom aut)) msx)
+                                       in addStates (gmsx ++ [msx], bmsx, mtrx) msx (map (\x -> filter (isEssential aut) x) (buildMetastate aut msx l)) l
+
+addStates :: ([MetaState], [MetaState], [MetaTransition]) -> MetaState -> [MetaState] -> [Label] -> ([MetaState], [MetaState], [MetaTransition])
+addStates x@(gmsx, bmsx, mtrx) msx mlx l = let mlxHead = head mlx
+                                               newBmsx = if elem mlxHead bmsx || elem mlxHead gmsx then bmsx else bmsx ++ [mlxHead]
+                                               newMtrx = mtrx ++ [(msx, mlxHead, head l)]
+                                           in if null mlx then x else addStates (gmsx, newBmsx ,newMtrx) msx (tail mlx) (tail l) 
+
+newNormSt :: ([MetaState], [MetaState], [MetaTransition]) -> MetaState -> [MetaState] -> [Label] -> ([MetaState], [MetaState], [MetaTransition])
+newNormSt (gmsx, bmsx, mtrx) msx mlx l |null mlx = (gmsx, bmsx, mtrx)
+                                       |otherwise = addStates (gmsx, newBmsx, newMtrx) msx (tail mlx) (tail l) 
+                                        where mlxHead = head mlx
+                                              newBmsx = if elem mlxHead bmsx || elem mlxHead gmsx then bmsx else bmsx ++ [mlxHead]
+                                              newMtrx = mtrx ++ [(msx, mlxHead, head l)]
+
+endCycleCond :: ([MetaState], [MetaState], [MetaTransition]) -> Bool
+endCycleCond (_, bmsx, _) = null bmsx
+
+comparator :: (Int, Int, Label) -> (Int, Int, Label) -> Ordering
+comparator (a1, b1, _) (a2, b2, _) | a1 > a2 = GT
+                                   | a1 < a2 = LT
+                                   | otherwise = compare b1 b2
+
 makeDA' :: Automation -> (MetaState, [MetaState], [MetaTransition])
-makeDA' = undefined  
+makeDA' aut = let (st, _, l) = until endCycleCond (makeStep aut)([],[filter (\x -> (isEssential aut x)) (closure aut [(getState aut)])],[]) in (head st, st, l)
 
 makeDA :: Automation -> Automation
-makeDA  = undefined
+makeDA aut = let (_, mstx, mtrx) = makeDA' aut
+                 endStates = [x + 1 | x <- [0..(length mstx) - 1], not (null (intersect (mstx !! x) (getEndStates aut)))]
+                 transitions = map (\(a, b, c) -> ((fromMaybe (-1) $ elemIndex a mstx) + 1, (fromMaybe (-1) $ elemIndex b mstx) + 1, c)) mtrx
+             in (1, endStates, (sortBy comparator transitions))
+
 -------------------------------------------------------
 -- showRE - function that can be useful while testing
 showRE :: RE -> String
